b2dc9178b4611fdf7d3a1104df078f1e66ff54e8
francooo32 <63883342+francooo32@users.noreply.github.com>
2023-03-30 01:49:38 -0300

    Fixed movement facing issues

--- ParallelShift/ParallelShift/Player.cpp
+++ ParallelShift/ParallelShift/Player.cpp
@@ -14,7 +14,7 @@ Player::Player(float x, float y, sf::Texture& texture_sheet)
 	//Configuration for sprite animation																								
 	this->animationComponent->addAnimation("IDLE", 0.1f, 0, 0, 0, 0, 192, 192);
 	this->animationComponent->addAnimation("WALK_LEFT", 1.f, 0, 2, 6, 2, 192, 192);
-	this->animationComponent->addAnimation("WALK_RIGHT", 1.f, 0, 1, 6, 1, 192, 192);
+	//this->animationComponent->addAnimation("WALK_RIGHT", 1.f, 0, 1, 6, 1, 192, 192);
 	this->animationComponent->addAnimation("WALK_UP", 1.f, 0, 1, 6, 1, 192, 192);
 	this->animationComponent->addAnimation("WALK_DOWN", 1.f, 0, 1, 6, 1, 192, 192);
 
@@ -34,11 +34,15 @@ void Player::update(const float& dt)
 	}
 	else if (this->movementComponent->getState(MOVING_LEFT))
 	{
+		this->sprite.setOrigin(0.f, 0.f);
+		this->sprite.setScale(1.f, 1.f);
 		this->animationComponent->play("WALK_LEFT", dt);
 	}
 	else if (this->movementComponent->getState(MOVING_RIGHT))
 	{
-		this->animationComponent->play("WALK_RIGHT", dt);
+		this->sprite.setOrigin(220.f, 0.f);
+		this->sprite.setScale(-1.f, 1.f);
+		this->animationComponent->play("WALK_LEFT", dt);
 	}
 	else if (this->movementComponent->getState(MOVING_UP))
 	{
